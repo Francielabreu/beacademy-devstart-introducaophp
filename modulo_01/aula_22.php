615248b08d2ac8bf4bfef6b692d25e2103d21f7d
Franciel Abreu <francielabreu23@gmail.com>
2022-06-02 23:07:03 -0300

    Lista de exercicios de PHP

---
+++ modulo_01/aula_22.php
@@ -0,0 +1,11 @@
+<?php
+$frutas = [
+    'banana',
+    'pera',
+    'laranja',
+    'uva',
+];
+
+foreach ($frutas as $posicao => $cadaFruta) {
+    echo "Posição : {$posicao} | fruta : {$cadaFruta}".PHP_EOL;
+}
