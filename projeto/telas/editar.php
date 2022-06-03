95f316bf4f7ab2af5c7f3051651398166f83c6fe
Franciel Abreu <francielabreu23@gmail.com>
2022-06-03 12:31:40 -0300

    Criação da tela editar

---
+++ projeto/telas/editar.php
@@ -0,0 +1,12 @@
+<h1>Editar Contato</h1>
+
+<hr>
+
+<form action="" method="post">
+    <input class="form-control mt-2" type="text" name="nome" value="<?php echo $dados[0];?>" placeholder="Informe seu nome">
+    <input class="form-control mt-2" type="text" name="email" value="<?php echo $dados[1];?>" placeholder="Informe sua Email">
+    <input class="form-control mt-2" type="text" name="telefone" value="<?php echo $dados[2];?>" placeholder="Informe seu Telefone"><br>
+
+    <button class="btn btn-primary" type="submit">Enviar</button>
+
+</form>
